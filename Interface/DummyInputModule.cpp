68cb7d4bd707eea191a724911238788e67c99fff
MartinUbl <cmaranec@seznam.cz>
2016-03-10 17:10:05 +0100

    Update dummy module to reflect latest changes

--- Interface/DummyInputModule.cpp
+++ Interface/DummyInputModule.cpp
@@ -62,38 +62,55 @@ void DummyInputModule::GetFunctionTable(std::vector<FunctionEntry> &dst)
 
     dst[0].name = "someClasslessFunction";
     dst[0].classId = NO_CLASS;
+    dst[0].functionType = FET_MISC;
 
     dst[1].name = "TestMethod";
     dst[1].classId = 0;
+    dst[1].functionType = FET_TEXT;
 
     dst[2].name = "MainAppMethod";
     dst[2].classId = 1;
+    dst[2].functionType = FET_TEXT;
 
     dst[3].name = "AnotherAppMethod";
     dst[3].classId = 1;
+    dst[3].functionType = FET_TEXT;
 
     dst[4].name = "SomeMethod";
     dst[4].classId = 2;
+    dst[4].functionType = FET_TEXT;
 }
 
 void DummyInputModule::GetFlatProfileData(std::vector<FlatProfileRecord> &dst)
 {
     dst.clear();
 
-    dst.resize(3);
+    dst.resize(4);
 
     dst[0].functionId = 2;
     dst[0].timeTotal = 4;
-    dst[0].timeTotalPct = 0.5f;
     dst[0].callCount = 10;
 
     dst[1].functionId = 3;
     dst[1].timeTotal = 3;
-    dst[1].timeTotalPct = 0.375f;
     dst[1].callCount = 5;
 
     dst[2].functionId = 1;
     dst[2].timeTotal = 1;
-    dst[2].timeTotalPct = 0.125f;
     dst[2].callCount = 8;
+
+    dst[3].functionId = 0;
+    dst[3].timeTotal = 3.5;
+    dst[3].callCount = 9;
+}
+
+void DummyInputModule::GetCallGraphMap(CallGraphMap &dst)
+{
+    dst.clear();
+
+    dst[0][1] = 5;
+    dst[1][2] = 4;
+    dst[1][3] = 8;
+    dst[0][3] = 1;
+    dst[3][2] = 4;
 }
--- Interface/DummyInputModule.h
+++ Interface/DummyInputModule.h
@@ -17,6 +17,7 @@ class DummyInputModule : public InputModule
         virtual void GetClassTable(std::vector<ClassEntry> &dst);
         virtual void GetFunctionTable(std::vector<FunctionEntry> &dst);
         virtual void GetFlatProfileData(std::vector<FlatProfileRecord> &dst);
+        virtual void GetCallGraphMap(CallGraphMap &dst);
 
     protected:
         //
